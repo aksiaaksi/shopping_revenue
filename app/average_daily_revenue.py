30236c18322a804a0c01b3b6cc20db00e782b152
aksi <a.k.sattarov@gmail.com>
2019-03-02 02:09:50 +0300

    added new test str

--- app/average_daily_revenue.py
+++ app/average_daily_revenue.py
@@ -19,6 +19,9 @@ def averages_daily(revenue_by_day):
 
     >>> averages_daily([[10], [5, 10, 15], [4, 8, 12, 16, 20, 24, 28]])
     16.0
+
+    >>> averages_daily([[5, 10, 15, 20, 25, 30, 35], [10, 20, 30, 40, 50, 60, 70], [4, 8, 12, 16, 20, 24, 28]])
+    40.0
     """
     summas = []  # empty list to save the averages daily revenue
 
