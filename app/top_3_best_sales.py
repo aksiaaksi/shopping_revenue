f33ef1493c79e5d5967aca24a006843e8114828c
aksi <a.k.sattarov@gmail.com>
2019-03-14 23:55:57 +0300

    Fixed #3

--- app/top_3_best_sales.py
+++ app/top_3_best_sales.py
@@ -1,16 +1,15 @@
 def best_sales(revenue_by_day:list):
 
+
     """
-    >>> best_sales([[10, 100, 400, 300], [50, 40, 200,1000], [4, 80, 12, 36]])
-    [[400, 300, 100], [1000, 200, 50], [80, 36, 12]]
+    >>> best_sales([[10, 100, 400, 300, 0, 0, 0], [50, 40, 200, 1000, 150, 0, 46], [4, 80, 12, 36, 0, 15, 16]])
+    [[100, 300, 400], [150, 200, 1000], [16, 36, 80]]
 
     """
     summa = []
 
     for average in revenue_by_day:
         average.sort()
-        average.reverse()
-        average[:3]
-        summa.append(average[:3])
+        summa.append(average[4:])
     return summa
 
