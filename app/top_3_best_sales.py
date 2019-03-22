2df77596f9de028c0856ea132e25cf9896b233e4
aksi <a.k.sattarov@gmail.com>
2019-03-22 15:56:15 +0300

    Corrected: average.sort() average.reverse()     ->average.sort(reverse = True) Fixed #3

--- app/top_3_best_sales.py
+++ app/top_3_best_sales.py
@@ -3,13 +3,13 @@ def best_sales(revenue_by_day:list):
 
     """
     >>> best_sales([[10, 100, 400, 300, 0, 0, 0], [50, 40, 200, 1000, 150, 0, 46], [4, 80, 12, 36, 0, 15, 16]])
-    [[100, 300, 400], [150, 200, 1000], [16, 36, 80]]
+    [[400, 300, 100], [1000, 200, 150], [80, 36, 16]]
 
     """
     summa = []
 
     for average in revenue_by_day:
-        average.sort()
-        summa.append(average[4:])
+        average.sort(reverse = True)
+        summa.append(average[:3])
     return summa
 
